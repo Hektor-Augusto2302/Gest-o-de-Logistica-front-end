1a018a38ef1b465b908f3a8ffa560f2379c0ed7f
Hektor-Augusto <hektoraugusto.s@gmail.com>
2025-02-27 12:08:46 -0300

    feat(adjustments in the mobile version)

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,46 +3,32 @@ import Illustration from "../../../public/login-illustration.svg";
 
 export default function LoginForm() {
     return (
-        <div className="flex items-center justify-center h-screen user-form">
-            <div className="flex w-[1300px] h-[6000px]overflow-hidden">
-                <div className="w-[30%] p-10 flex flex-col justify-center">
-                    <h1 className="flex text-3xl font-bold text-center">Login</h1>
-                    <p className="flex text-center text-gray-600 mt-2">
+        <div className="flex items-center justify-center h-screen px-4 py-8 user-form">
+            <div className="flex flex-col lg:flex-row w-full max-w-[1300px] overflow-hidden">
+                <div className="lg:w-[30%] p-6 md:p-10 flex flex-col justify-center">
+                    <h1 className="text-3xl font-bold">Login</h1>
+                    <p className="text-gray-600 mt-2">
                         Veja seu crescimento e obtenha suporte
                     </p>
                     <form className="mt-6">
                         <div className="flex flex-col">
                             <label className="label-form">E-mail</label>
-                            <input
-                                type="email"
-                                name="email"
-                                className="input-form"
-                                required
-                            />
+                            <input type="email" name="email" className="input-form" required />
                         </div>
                         <div className="flex flex-col mt-4">
                             <label className="label-form">Senha</label>
-                            <input
-                                type="password"
-                                name="password"
-                                className="input-form"
-                                required
-                            />
+                            <input type="password" name="password" className="input-form" required />
                         </div>
                         <div className="flex justify-center mt-6">
-                            <input
-                                type="submit"
-                                className="w-full button-form"
-                                value="Entrar"
-                            />
+                            <input type="submit" className=" button-form w-full" value="Entrar" />
                         </div>
                     </form>
                 </div>
-                <div className="w-[70%] flex items-center justify-center">
+                <div className="w-full lg:w-[70%] flex items-center justify-center mt-6 lg:mt-0">
                     <Image
                         src={Illustration}
                         alt="Ilustração de login"
-                        className="w-[100%] h-auto"
+                        className="md:w-full md:max-w-none w-[90%] max-w-[500px] h-auto"
                     />
                 </div>
             </div>
