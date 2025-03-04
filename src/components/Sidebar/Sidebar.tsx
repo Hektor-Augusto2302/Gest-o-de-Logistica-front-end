decc949f3d1de2a619f24d350b2d4a644bcf4790
Hektor-Augusto <hektoraugusto.s@gmail.com>
2025-03-04 09:59:46 -0300

    fix(fixing the sidebar styles)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,39 +11,46 @@ interface SidebarProps {
 export default function Sidebar({ isOpen }: SidebarProps) {
     return (
         <aside
-            className={`fixed top-16 left-0 h-[calc(100vh-4rem)] p-4 transition-all duration-300 bg-gray-900 text-white
+            className={`fixed top-16 left-0 h-[calc(100vh-4rem)] p-4 transition-all 
+                duration-300 bg-gray-900 text-white
                 ${isOpen ? "w-40" : "w-16"}
             `}
         >
             <nav className="mt-8 flex flex-col space-y-4">
-                <Link href="/" className="flex text-white flex-col items-center hover:text-gray-400 transition">
+                <Link href="/" className="flex text-white flex-col items-center hover:text-gray-400 
+                    transition no-underline"
+                >
                     <Image 
                         src={DashboardImage} 
                         width={24}
                         height={24}
                         alt="Imagem da dashboard"
                     />
-                    {isOpen && <span className="text-sm">Dashboard</span>}
+                    {isOpen && <span className="text-sm mt-3">Dashboard</span>}
                 </Link>
 
-                <Link href="/about" className="flex text-white flex-col items-center hover:text-gray-400 transition">
+                <Link href="/about" className="flex text-white flex-col items-center hover:text-gray-400 
+                    transition no-underline"
+                >
                     <Image 
                         src={DashboardImage} 
                         width={24}
                         height={24}
                         alt="Imagem da dashboard"
                     />
-                    {isOpen && <span className="text-sm">Sobre</span>}
+                    {isOpen && <span className="text-sm mt-3">Sobre</span>}
                 </Link>
 
-                <Link href="/contact" className="flex text-white flex-col items-center hover:text-gray-400 transition">
+                <Link href="/contact" className="flex text-white flex-col items-center hover:text-gray-400 
+                    transition no-underline"
+                >
                     <Image 
                         src={DashboardImage} 
                         width={24}
                         height={24}
                         alt="Imagem da dashboard"
                     />
-                    {isOpen && <span className="text-sm">Contato</span>}
+                    {isOpen && <span className="text-sm mt-3">Contato</span>}
                 </Link>
             </nav>
         </aside>
